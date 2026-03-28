4631d088eb748a63cbf98aa6230c79f3c3fd8a65
Judel Bagisan <judelcabahugbagisan@gmail.com>
2026-03-28 16:46:22 +0800

    added sitemap features

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://judelbagisan.dev';
+  const lastModified = new Date();
+
+  return [
+    {
+      url: baseUrl,
+      lastModified,
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/allprojects`,
+      lastModified,
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+  ];
+}
